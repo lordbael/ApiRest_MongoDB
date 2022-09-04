c3cb9064fd9f44c07555de0919fdcc45c6b9ddfa
Juan Carlos <80911763+lordbael@users.noreply.github.com>
2022-09-04 14:19:39 -0500

    Subida de la Api Rest

---
+++ src/main/java/com/hg/crud/repository/IProductDAO.java
@@ -0,0 +1,10 @@
+package com.hg.crud.repository;
+
+import com.hg.crud.models.ProductDTO;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProductDAO extends MongoRepository<ProductDTO, String> {
+
+}
